5712833d779ebcec243c8fcbfe84cb5e5f75ca59
Naem Azam <78720392+naemazam@users.noreply.github.com>
2023-02-22 14:11:18 +0800

    Update shaheed_minar.py

--- shaheed_minar.py
+++ shaheed_minar.py
@@ -5,164 +5,182 @@
 ########################################################################
 
 import turtle
-from shapes import Rectangle
-from pillar import Pillar
+
+dpen=turtle.Turtle()
+dpen.width(3)
+dpen.color("white")
+new=turtle.getscreen()
+dpen.speed(30)
+
+new.bgcolor("black")
+
+# Hidden Work(penup)
+dpen.left(180)
+dpen.penup()
+dpen.forward(270)
+dpen.left(90)
+dpen.forward(200)
+dpen.left(180)
+dpen.pendown()
+
+dpen.pendown()
 
 
-def mainPillar(x, y, bottom_height, top_height, width, thickness):
-    # Draw bottom column 1
-    col1 = Rectangle(x, y, bottom_height, thickness)
-    col1.draw()
+# Printing 1
 
-    # Draw bottom column 2
-    x, y = col1.bottom_right
-    col2 = Rectangle(x + width, y, bottom_height, thickness)
-    col2.draw()
-    
-    # Draw rods inside col1 and col2
-    roddist = width / 3
-    x, y = col1.bottom_right
-    draw_rods(x, y, bottom_height, roddist)
-   
-    # Draw bottom column 3
-    x, y = col2.bottom_right
-    col3 = Rectangle(x + width, y, bottom_height, thickness)
-    col3.draw()
-    
-    # Draw rods inside col2 and col3
-    x, y = col2.bottom_right
-    draw_rods(x, y, bottom_height, roddist)
+#start to draw
+dpen.forward(200)
+dpen.right(90)
+dpen.forward(55)
+dpen.right(90)
+dpen.forward(200)
+dpen.right(90)
+dpen.forward(10)
+dpen.right(90)
+dpen.forward(190)
+dpen.left(90)
+dpen.forward(35)
+dpen.left(90)
+dpen.forward(190)
+dpen.right(90)
+dpen.forward(10)
 
-    x1, y1 = col1.top_left
-    x2, y2 = col3.top_left
+dpen.penup()
+dpen.left(180)
+dpen.forward(75)
+dpen.left(90)
 
-    turtle.penup()
-    turtle.setpos(x1, y1)
-    turtle.setheading(0)
-    turtle.pendown()
-    turtle.forward(turtle.distance(x2, y2))
-
-    # Draw top column 1
-    x, y = col1.top_left
-    col4 = Rectangle(x, y, top_height, thickness)
-    col4.draw_right(30)
-    
-    # Draw top column 2
-    x, y = col2.top_left
-    col5 = Rectangle(x, y, top_height, thickness)
-    col5.draw_right(30)
-
-    # Draw rods inside col4 and col5
-    x, y = col4.bottom_right
-    draw_rods(x, y, top_height, roddist, -30)
-    
-    # Draw top column 3
-    x, y = col2.top_left
-    x, y = col3.top_left
-    col6 = Rectangle(x, y, top_height, thickness)
-    col6.draw_right(30)
-    
-    # Draw rods inside col5 and col6
-    x, y = col5.bottom_right
-    draw_rods(x, y, top_height, roddist, -30)
-
-    x, y = col4.top_left
-    
-    beam1 = Rectangle(x, y+1, thickness, width * 2 + 30)
-    beam1.draw_right(30)
-    
-    # Remove intersection of coloumn 4 and beam 1
-    x, y = beam1.bottom_left
-    print((x, y))
-    beam1.erase((x + 1, y), (x + thickness - 1, y))
-   
-    # Remove intersection of coloumn 5 and beam 1 
-    x, y = col5.top_left
-    print((x, y))
-    beam1.erase((x + 1, y + 1), (x + thickness + 2, y))
-
-    # Remove intersection of coloumn 5 and beam 1 
-    x, y = col6.top_left
-    print((x, y))
-    beam1.erase((x + 1, y + 1), (x + thickness + 2, y))
-
-    return col3.bottom_right
-
-# Draws rods inside the pillars
-def draw_rods(x, y, height, roddist, angle=0):
-    turtle.penup()
-    turtle.setpos(x + roddist, y)
-    turtle.pendown()
-    turtle.setheading(0)
-    turtle.left(90+angle)
-    turtle.forward(height)
-    
-    turtle.penup()
-    turtle.setpos(x + (2 * roddist), y)
-    turtle.pendown()
-    turtle.setheading(0)
-    turtle.left(90+angle)
-    turtle.forward(height)
+dpen.pendown()
 
 
-if __name__ == "__main__":
-    turtle.hideturtle()
-    turtle.bgcolor("black")
-    turtle.color("green")
 
-    start_x = -280
-    start_y = -150
-    height = 100
-    width = 60
-    thickness = 10
-    roddist = width / 3
+# Printing 2
 
-    # Draw first small pillar
-    p1 = Pillar(start_x, start_y, height, width, thickness)
-    p1.draw()
-    p1.prettify()
-    draw_rods(start_x, start_y, height, roddist)
-    
-    # Draw second small pillar
-    x, y = p1.far_right()
-    
-    p2 = Pillar(x + 20, y, height * 2, width, thickness)
-    p2.draw()
-    p2.prettify()
+dpen.forward(270)
+dpen.right(90)
+dpen.forward(55)
+dpen.right(90)
+dpen.forward(270)
+dpen.right(90)
+dpen.forward(10)
+dpen.right(90)
+dpen.forward(260)
+dpen.left(90)
+dpen.forward(35)
+dpen.left(90)
+dpen.forward(260)
+dpen.right(90)
+dpen.forward(10)
 
-    # Draw rods inside second pillar
-    x, y = p2.far_left()
-    draw_rods(x, y, height * 2, roddist)
+dpen.penup()
+dpen.left(180)
+dpen.forward(110)
+dpen.left(90)
 
-    # Draw main pillar
-    x, y = p2.far_right()
+dpen.pendown()
 
-    x, y = mainPillar(x + 20, y, height * 3, height, width + 20, thickness)
-   
-    # Draw third small pillar
-    p3 = Pillar(x + 20, y, height * 2, width, thickness)
-    p3.draw()
-    p3.prettify()
 
-    # Draw rods inside third pillar
-    x, y = p3.far_left()
-    draw_rods(x, y, height * 2, roddist)
 
-    # Draw fourth small pillar
-    x, y = p3.far_right()
-    
-    p4 = Pillar(x + 20, y, height, width, 10)
-    p4.draw()
-    p4.prettify()
+# Printing 3
+dpen.forward(290)
+dpen.left(15)
+dpen.forward(150)
+dpen.right(105)
+dpen.forward(252.6457135)
+dpen.right(105)
+dpen.forward(150)
+dpen.left(15)
+dpen.forward(290)
+dpen.right(90)
+dpen.forward(15)
+dpen.right(90)
+dpen.forward(290)
+dpen.right(15)
+dpen.forward(140)
+dpen.left(105)
+dpen.forward(125.5454112)
+dpen.left(100)
+dpen.forward(140)
+dpen.right(10)
+dpen.forward(290)
+dpen.right(90)
+dpen.forward(15)
+dpen.right(90)
+dpen.forward(290)
+dpen.left(10)
+dpen.forward(140)
+dpen.left(80)
+dpen.forward(76.74131845)
+dpen.left(105)
+dpen.forward(140)
+dpen.right(15)
+dpen.forward(290)
+dpen.right(90)
+dpen.forward(15)
 
-    # Draw rods inside fourth small pillar
-    x, y = p4.far_left()
-    draw_rods(x, y, height, roddist)
+dpen.penup()
+dpen.left(180)
+dpen.forward(225)
+dpen.left(90)
 
-    turtle.penup()
-    turtle.setheading(0)
-    turtle.setpos(-350, -150)
-    turtle.pendown()
-    turtle.forward(turtle.distance(350, -150))
+dpen.pendown()
 
-    x = raw_input()
+
+
+
+# Printing 4
+
+dpen.forward(270)
+dpen.right(90)
+dpen.forward(55)
+dpen.right(90)
+dpen.forward(270)
+dpen.right(90)
+dpen.forward(10)
+dpen.right(90)
+dpen.forward(260)
+dpen.left(90)
+dpen.forward(35)
+dpen.left(90)
+dpen.forward(260)
+dpen.right(90)
+dpen.forward(10)
+
+dpen.penup()
+dpen.left(180)
+dpen.forward(75)
+dpen.left(90)
+
+dpen.pendown()
+
+
+
+# Printing 1
+
+dpen.forward(200)
+dpen.right(90)
+dpen.forward(55)
+dpen.right(90)
+dpen.forward(200)
+dpen.right(90)
+dpen.forward(10)
+dpen.right(90)
+dpen.forward(190)
+dpen.left(90)
+dpen.forward(35)
+dpen.left(90)
+dpen.forward(190)
+dpen.right(90)
+dpen.forward(10)
+
+
+
+dpen.penup()
+dpen.left(90)
+dpen.forward(40)
+dpen.right(90)
+dpen.forward(360)
+dpen.left(180)
+dpen.forward(142.5)
+
+turtle.done()
